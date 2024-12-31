3e0db3edcabc94c842ba7b6995178e56cc5a38e3
Joachim Leonfellner <nor0x@users.noreply.github.com>
2024-12-31 14:08:25 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+		private void OnDrop(object sender, DropEventArgs e)
+		{
+            Debug.WriteLine($"Dropped {e.Data}");
+		}
+    }
+
+}
